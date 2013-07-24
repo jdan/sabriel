0896e87b612d7714dba36682c9bdfaafdc474669
Jordan Scales <scalesjordan@gmail.com>
2013-07-23 20:38:45 -0700

    Change card color based on reaction (detected on keyup)

--- client/views/cards/card_entry.js
+++ client/views/cards/card_entry.js
@@ -8,12 +8,24 @@ Template.cardEntry.events({
 
     if (!/^\s*$/.test(card.content)) {
       $(e.target).find('[name=content]').val('')
+
+      // Reset the class
+      $('.card-entry').removeClass(['positive', 'neutral', 'negative'].join(' '))
+                      .addClass('positive')
       
       Meteor.call('card', card, function (error, id) {
         if (error)
           alert(error.reason)
       })
     }
+  },
 
+  'keyup input[name=content]': function (e) {
+    var content = $(e.target).val()
+    var type = reaction(content)
+
+    // Change the entry field to have the appropriate class
+    $('.card-entry').removeClass(['positive', 'neutral', 'negative'].join(' '))
+                    .addClass(type)
   }
 })
