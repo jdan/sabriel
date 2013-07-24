195dd0b1c3f4f6887778c521694951e121bc8793
Jordan Scales <scalesjordan@gmail.com>
2013-07-23 20:24:23 -0700

    Moved card type detection to server

--- client/views/cards/card_entry.js
+++ client/views/cards/card_entry.js
@@ -2,22 +2,8 @@ Template.cardEntry.events({
   'submit form': function (e) {
     e.preventDefault()
 
-    var content = $(e.target).find('[name=content]').val()
-    var type = 'positive'
-
-    if (/^:\(\s+/.test(content)) {
-      type = 'negative'
-    } else if (/^:\)\s+/.test(content)) {
-      type = 'positive'
-    } else if (/^:\|\s+/.test(content)) {
-      type = 'neutral'
-    }
-
-    var extract = content.match(/(?:\:[\(\)|]\s+)(.*)/)
-
-    card = {
-      content: (extract && extract[1]) ? extract[1] : content,
-      type: type
+    var card = {
+      content: $(e.target).find('[name=content]').val()
     }
 
     if (!/^\s*$/.test(card.content)) {
@@ -28,5 +14,6 @@ Template.cardEntry.events({
           alert(error.reason)
       })
     }
+
   }
 })
--- collections/card.js
+++ collections/card.js
@@ -9,6 +9,7 @@ Cards.allow({
 Meteor.methods({
   card: function (cardAttributes) {
     var user = Meteor.user()
+    var type = 'positive'
 
     // ensure the user is logged in
     if (!user)
@@ -18,12 +19,20 @@ Meteor.methods({
     if (/^\s*$/.test(cardAttributes.content))
       throw new Meteor.Error(422, "Please fill in a card");
 
+    if (/^:\(\s+/.test(cardAttributes.content)) {
+      type = 'negative'
+    } else if (/^:\)\s+/.test(cardAttributes.content)) {
+      type = 'positive'
+    } else if (/^:\|\s+/.test(cardAttributes.content)) {
+      type = 'neutral'
+    }
+
+    var extract = cardAttributes.content.match(/(?:\:[\(\)|]\s+)(.*)/)
+
     // build the message
     var card = {
-      // watercoolerFeatures() includes markdown, images, emoji, etc.
-      // see `client/helpers/string.js` for details
-      content: cardAttributes.content,
-      type: cardAttributes.type,
+      content: (extract && extract[1]) ? extract[1] : cardAttributes.content,
+      type: type,
       userId: user._id,
       submitted: new Date().getTime()
     };
