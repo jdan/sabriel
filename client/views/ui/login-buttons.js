36e1a36a70109a300f58fbb8073eae1b743ff71c
Jordan Scales <scalesjordan@gmail.com>
2013-07-27 16:10:52 -0700

    Logout button - fixes #10

--- client/views/ui/login-buttons.js
+++ client/views/ui/login-buttons.js
@@ -1,12 +1,19 @@
 Template.loginButton.events({
   'click button.login': function (e) {
     e.preventDefault()
+    if (Meteor.user()) return
     Meteor.loginWithGoogle({
       requestPermissions: ['email']
-    },
-    function (err) {
+    }, function (err) {
       if (err)
         Session.set('errorMessage', err.reason || 'Unknown error');
     })
+  }
+})
+
+Template.logoutButton.events({
+  'click button.logout': function (e) {
+    e.preventDefault()
+    if (Meteor.user()) Meteor.logout()
   }
 })
